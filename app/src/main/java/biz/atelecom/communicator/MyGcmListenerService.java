451201131a6b8a7d120d814517b71fcec738e977
vasylignatyev <vasyl.ignatyev@gmail.com>
2016-03-30 18:00:37 +0300

    30.04.2016

--- app/src/main/java/biz/atelecom/communicator/MyGcmListenerService.java
+++ app/src/main/java/biz/atelecom/communicator/MyGcmListenerService.java
@@ -81,8 +81,8 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("GCM Message")
+                .setSmallIcon(R.drawable.atelecom_ic_notification)
+                .setContentTitle("Atelecom Message")
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
